c842eb4fbd345be943971aac696156838ae5dd3b
Vendula Bezáková <vendula.bezakova@gmail.com>
2024-06-30 17:31:58 +0200

    zobrazení ingrediencí v menu na hl. stránce

--- src/components/Drink/drink.jsx
+++ src/components/Drink/drink.jsx
@@ -1,11 +1,16 @@
 import './style.css';
 import { Layer } from '../Layer/layer.jsx';
 
-export const Drink = ({ id, name, ordered, image, layers }) => {
+export const Drink = ({ name, image, layers }) => {
   return (
     <div className="drink">
       <h2>{name}</h2>
       <img src={image} alt={name} />
+      <div className="layers">
+        {layers.map((layer, index) => (
+          <Layer key={index} color={layer.color} label={layer.label} />
+        ))}
+      </div>
     </div>
   );
 };
